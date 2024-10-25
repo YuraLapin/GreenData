1da67a7f72e3e8b6d5be68768d461b98be8bc4c9
PostnikovEvgeny <postnikoff.evgeny2015@yandex.ru>
2024-10-25 20:36:52 +0500

    Creating project

---
+++ GoogleDiskGreenDataIntegration/src/main/java/com/example/GoogleDiskGreenDataIntegration/GoogleDiskGreenDataIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.GoogleDiskGreenDataIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleDiskGreenDataIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleDiskGreenDataIntegrationApplication.class, args);
+	}
+
+}
---
+++ GoogleDiskGreenDataIntegration/src/test/java/com/example/GoogleDiskGreenDataIntegration/GoogleDiskGreenDataIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GoogleDiskGreenDataIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleDiskGreenDataIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
