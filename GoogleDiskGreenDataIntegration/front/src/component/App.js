558d1a0cdb842a3015e2bf93cda3f67d82356885
Yura Lapin <8chewbacca@gmail.com>
2024-12-09 11:51:36 +0500

    Починил ошибку при пустом localStorage

--- GoogleDiskGreenDataIntegration/front/src/component/App.js
+++ GoogleDiskGreenDataIntegration/front/src/component/App.js
@@ -41,7 +41,7 @@ export default function App() {
                     </div>
                 )
             }) }
-            { token.length > 0 && <button onClick={logOut}>Log out</button> }
+            { token && token.length > 0 && <button onClick={logOut}>Log out</button> }
         </>
     )
 }
