69b854c2431997c09a3d897284a1bdcb4ccd4881
PostnikovEvgeny <postnikoff.evgeny2015@yandex.ru>
2024-12-13 12:14:34 +0500

    скачивание файла из гугл диска

--- GoogleDiskGreenDataIntegration/src/main/java/com/example/GoogleDiskGreenDataIntegration/controller/GoogleDriveController.java
+++ GoogleDiskGreenDataIntegration/src/main/java/com/example/GoogleDiskGreenDataIntegration/controller/GoogleDriveController.java
@@ -1,7 +1,6 @@
 package com.example.GoogleDiskGreenDataIntegration.controller;
 
 import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
@@ -10,18 +9,28 @@ import com.google.api.services.drive.Drive;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Arrays;
+
 
 import java.io.*;
-import java.net.http.HttpResponse;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api")
 public class GoogleDriveController {
 
-    private final String DOWNLOAD_DIR = "Z:\\GreenDataProject\\09,12\\GreenData\\GoogleDiskGreenDataIntegration\\src\\main\\resources"; // Path to the local storage
+    private final String DOWNLOAD_DIR = "F:\\TEST";
 
     @PostMapping("/upload")
     public ResponseEntity<String> downloadFile(@RequestParam String fileId, @RequestParam String accessToken) {
@@ -36,17 +45,23 @@ public class GoogleDriveController {
                     .build();
 
             Drive.Files.Get request = driveService.files().get(fileId);
-            
 
-            File localFile = new File(DOWNLOAD_DIR + "/" + "123");
-            OutputStream outputStream = new FileOutputStream(localFile);
+            com.google.api.services.drive.model.File file = request.execute();
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
             request.executeMediaAndDownloadTo(outputStream);
-            outputStream.close(); 
 
-            return ResponseEntity.ok("File downloaded successfully to: " + localFile.getAbsolutePath());
+            File localFile = new File(DOWNLOAD_DIR + "\\" + file.getName());
+
+            try (FileOutputStream fileOutputStream = new FileOutputStream(localFile)) {
+                outputStream.writeTo(fileOutputStream);
+            }
+
+            return ResponseEntity.ok("Файл скачан успешно в: " + localFile.getAbsolutePath());
         } catch (Exception e) {
             e.printStackTrace();
-            return ResponseEntity.status(500).body("Failed to download the file: " + e.getMessage());
+            return ResponseEntity.status(500).body("Ошибка скачивания файла: " + e.getMessage());
         }
     }
 }
